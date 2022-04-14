0c3d858db52924921a6462b8c5da8b04eae5e770
MathEnthusiasPi <89387162+MathEnthusiast314@users.noreply.github.com>
2022-04-14 14:10:29 +0000

    previous commit 2

--- feature/recGraph.py
+++ feature/recGraph.py
@@ -20,8 +20,8 @@ async def recGraphE(message,hash,theauthor=None,first='!!!'):
       user=message.author
       embed=nextcord.Embed(title=str(geti['title']),description=link0)
       if hash in GraphsList and objowner.get(str(hash),None) is not None:
-        if user.id in [468963362131279882,686012491607572515] and theauthor is not None:
-          embed.set_author(name=str(theauthor))
+        if user.id in [468963362131279882,686012491607572515]:
+          embed.set_author(name=(str(user) if theauthor is None else theauthor))
         else:
           pass
       elif theauthor is None:
@@ -48,8 +48,8 @@ async def recGraphE(message,hash,theauthor=None,first='!!!'):
         Variables.thetitles[str(hash)]=str(geti['title'])
       if first=='!!!' and objowner.get(str(hash),None) is None and '!https://www.desmos.com/calculator' not in message.content:
         Variables.objowner[str(hash)]=(str(user) if theauthor is None else theauthor)+'<@'+str(user.id)+'>'
-      if first=='!!!' and objowner.get(str(hash),None) is not None and '!https://www.desmos.com/calculator' not in message.content and hash in GraphsList and user.id in [468963362131279882,686012491607572515] and theauthor is not None:
-        Variables.objowner[str(hash)]=(str(theauthor))+'<@'+str(user.id)+'>'
+      if first=='!!!' and objowner.get(str(hash),None) is not None and '!https://www.desmos.com/calculator' not in message.content and hash in GraphsList and user.id in [468963362131279882,686012491607572515]:
+        Variables.objowner[str(hash)]=(str(user) if theauthor is None else theauthor)+'<@'+str(user.id)+'>'
       #
       if geti['parent_hash'] is not None:
         await recGraphE(message,geti['parent_hash'],theauthor,first='')
@@ -71,11 +71,11 @@ async def recGraphE(message,hash,theauthor=None,first='!!!'):
         graphcard=await damsg0.edit(content=damsg0.content+'!!!!',embed=embed00)
         Variables.objowner[str(hash)]=(str(user) if theauthor is None else theauthor)+'<@'+str(user.id)+'>'
         return graphcard
-      elif '!!!' in damsg0.content and hash in GraphsList and objowner.get(str(hash),None) is not None and user.id in [468963362131279882,686012491607572515] and theauthor is not None:
+      elif '!!!' in damsg0.content and hash in GraphsList and objowner.get(str(hash),None) is not None and user.id in [468963362131279882,686012491607572515]:
         embed00=damsg0.embeds[0]
-        embed00.set_author(name=str(theauthor))
+        embed00.set_author(name=(str(user) if theauthor is None else theauthor))
         graphcard=await damsg0.edit(content=damsg0.content,embed=embed00)
-        Variables.objowner[str(hash)]=(str(theauthor))
+        Variables.objowner[str(hash)]=(str(user) if theauthor is None else theauthor)+'<@'+str(user.id)+'>'
         return graphcard
         
 
