b6dfe73da1ea4761dfb8a7326ec4de0273d4ac42
MathEnthusiasPi <89387162+MathEnthusiast314@users.noreply.github.com>
2022-05-21 11:50:28 +0000

    no inflation of descoin

--- feature/profile.py
+++ feature/profile.py
@@ -74,7 +74,7 @@ async def give(message):
   if len(ifany)==1:
     giver=await channel.fetch_message(ifany[0])
     amount=int(0 if giver.embeds[0].fields[0].value=='∞' else giver.embeds[0].fields[0].value)
-    if much*len(to)<amount or (message.author.id==686012491607572515 or message.author.id==client.user.id):
+    if much*len(to)<amount or message.author.id==client.user.id:
       #
       await getready(message)
       RecMsg = await record(message)
@@ -86,15 +86,20 @@ async def give(message):
           ifanyuser=[i[0] for i in dpfplist if i[1]==(user.id)]
 
           if len(ifanyuser)==1:
+            if message.author.id==686012491607572515 and int(p)==686012491607572515:
+              pass
+            else:
+              await appendamount(message,ifany[0],-much)
             await appendamount(message,ifanyuser[0],much)
-            await appendamount(message,ifany[0],-much)
+            
           else:
-            await defaultpfp(user,much)
             await appendamount(message,ifany[0],-much)
+            await defaultpfp(user,much)
         except:
           if p!=str(client.user.id):
             await message.reply("{} is an invalid user id".format(p))
       await message.add_reaction('✅')
+      await RecMsg.add_reaction('✅')
     else:
       await message.reply("You don't have enough descoins to !give")
   else:
@@ -112,7 +117,7 @@ async def defaultpfp(user,much):
 
 async def appendamount(message,user000,amount):
   channel = client.get_channel(Dprofilechannel)
-  if (message.author.id==686012491607572515 or message.author.id==client.user.id) and amount<0:
+  if message.author.id==client.user.id and amount<0:
     pass
   else:
     damsg0=await channel.fetch_message(user000)
